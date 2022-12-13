99b665e104733a638623e26ea521e02a3eaee249
superderped <24651666+superderped@users.noreply.github.com>
2022-12-13 15:56:34 +1100

    Calculator Final Build
    Debugged all components and added text

---
+++ script.js
@@ -0,0 +1,78 @@
+displayPrev = document.querySelector('.displayPrev');
+display = document.querySelector('.display');
+nums = document.querySelectorAll('.num');
+operations = document.querySelectorAll('.operation');
+clear = document.querySelector('.clear');
+equal = document.querySelector('.equal');
+console.log(nums)
+
+function input(e){
+    x += this.textContent;
+    display.textContent = x;
+};
+
+function operate(e){
+    if ((currentOperation != "") & (x !="")){
+        equalHandler(e);
+        currentOperation = this.textContent;
+    } else if (x !=""){
+        currentOperation = this.textContent;
+        y = display.textContent;
+        displayPrev.textContent = y;
+        x = "";
+        display.textContent = "0";
+    }
+    console.log("x: "+x);
+    console.log("y: "+y); 
+    console.log("OP: "+currentOperation);
+    
+}
+
+function clearHandler(e){
+    reset();
+    currentOperation = "";
+    displayPrev.textContent = "";
+    display.textContent = "0";
+}
+
+function reset(){
+    x = "";
+    y = "";
+}
+
+function equalHandler(e){
+    if (x == ""){
+        x = "0";
+    }
+    if (currentOperation == "+"){
+        y = parseInt(y)+parseInt(x);
+    } else if (currentOperation == "-"){
+        y = parseInt(y)-parseInt(x);
+    } else if (currentOperation == "*"){
+        y = parseInt(y)*parseInt(x);
+    } else if (currentOperation == "÷"){
+        y = parseInt(y)/parseInt(x);
+    }
+    x = "";
+    //currentOperation = "";
+    displayPrev.textContent = y;
+    display.textContent = "0";
+    console.log("x: "+x);
+    console.log("y: "+y); 
+    console.log("OP: "+currentOperation);
+}
+
+nums.forEach(element => {
+    element.addEventListener("click", input);
+});
+
+operations.forEach(element => {
+    element.addEventListener("click", operate);
+});
+
+equal.addEventListener('click', equalHandler);
+clear.addEventListener('click', clearHandler);
+
+let x = "";
+let y = "";
+let currentOperation = "";
